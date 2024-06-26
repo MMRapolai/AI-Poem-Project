16861e65d6998c57f1ce9f54bd7327e612a20ae8
Matshelane Rapolai <matshelanerapolai@gmail.com>
2024-06-26 21:36:21 +0200

    added basic form handling JS and typewriter

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("Hello!");
+function generatePoem (event){
+    event.preventDefault();
+
+    new Typewriter("#poem",{
+        strings: "English poem, i love flowers!",
+        autoStart: true,
+        delay: 2,
+        cursor:"",});}
+
+let poemFormElement = document.querySelector("#poem-form-generator");
+poemFormElement.addEventListener("submit", generatePoem);
+        
+
